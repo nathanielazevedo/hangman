2e7a368af283592869eb90bb18e02627f501851a
nateazevedo <nathanielazevedo@hotmail.com>
2023-04-25 22:36:48 -0700

    basic implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import hangman from "./assets/hangman.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [guessed, setGuessed] = useState([]);
+  const [input, setInput] = useState("");
+  const [wrongs, setWrongs] = useState(0);
+  const word = "hello";
 
   return (
     <>
+      <div
+        className="App"
+        style={{
+          marginBottom: "50px",
+        }}
+      >
+        <h1>Hangman</h1>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={hangman} width={200} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {word.split("").map((letter) => {
+          return (
+            <div
+              style={{
+                border: "1px solid white",
+                textAlign: "center",
+                width: "50px",
+                height: "50px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin: "5px",
+              }}
+            >
+              <h5
+                style={{
+                  color: "white",
+                }}
+              >
+                {guessed.includes(letter) ? letter : ""}
+              </h5>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div
+        style={{
+          marginTop: "70px",
+        }}
+      >
+        <div>Guess a Letter</div>
+        <input
+          type="text"
+          value={input}
+          onChange={(evt) => {
+            setInput(evt.target.value);
+            setGuessed((guessed) => [...guessed, evt.target.value]);
+            if (word.includes(evt.target.value)) {
+              console.log("correct");
+            } else {
+              console.log("incorrect");
+            }
+            setInput("");
+          }}
+        />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
